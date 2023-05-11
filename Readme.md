eaabbecc963f4c4a131f4b7c3bdcae9b3b30be10
Oleg Fursa <olegfursa93@hotmail.com>
2023-05-11 17:23:35 +0100

    Provision networking on EC2 for app

--- Readme.md
+++ Readme.md
@@ -85,3 +85,189 @@ variable "<name>"{
 ## Create a Hybrid infrastructure with a VPC
 
 ![Hybrid diagram](resources/TerraformHybrid.JPG)
+
+### Create a VPC
+
+Use the following code to create a new VPC:
+```
+# Create a VPC on AWS
+
+resource "aws_vpc" "tech221_oleg_terraform_vpc" {
+  cidr_block = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "tech221_oleg_terraform_vpc"
+  }
+}
+```
+
+### Add a Gateway
+```
+# create a Gateway on AWS
+resource "aws_internet_gateway" "tech221_oleg_terraform_gw" {
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  tags = {
+    Name = "tech221_oleg_terraform_gw"
+  }
+}
+```
+
+### Add a Public Route Table
+
+```
+# Create a public route table on AWS
+resource "aws_route_table" "tech221_oleg_publicRT" {
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tech221_oleg_terraform_gw.id
+  }
+
+  tags = {
+    Name = "oleg_publicRT"
+  }
+}
+```
+
+### Create a Private Route Table
+```
+# Create a private route table on AWS
+resource "aws_route_table" "tech221_oleg_privateRT" {
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  tags = {
+    Name = "oleg_privateRT"
+  }
+}
+```
+
+### Create a Public subnet
+```
+# Create a Public Subnet in VPC
+
+resource "aws_subnet" "tech221_oleg_publicSubnet" {
+	vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+	cidr_block = var.public_subnet_cidr_block
+	map_public_ip_on_launch = "true"
+	availability_zone = var.availability_zone
+
+	tags = {
+		Name = "tech221_oleg_publicSubnet"
+	}
+
+}
+```
+
+### Create a Private Subnet
+```
+# Create a Private Subnet in VPC
+
+resource "aws_subnet" "tech221_oleg_privateSubnet" {
+	vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+	cidr_block = var.private_subnet_cidr_block
+	availability_zone = var.availability_zone
+
+	tags = {
+		Name = "tech221_oleg_privateSubnet"
+	}
+
+}
+```
+
+### Associate Public subnet to Public RT
+
+```
+# Creating Route association public Subnet
+
+resource "aws_route_table_association" "oleg_public_association" {
+	subnet_id = aws_subnet.tech221_oleg_publicSubnet.id
+	route_table_id = aws_route_table.tech221_oleg_publicRT.id
+}
+```
+
+### Associate Private subnet to Private RT
+
+```
+# Creating Route association private Subnet
+
+resource "aws_route_table_association" "oleg_private_association" {
+	subnet_id = aws_subnet.tech221_oleg_privateSubnet.id
+	route_table_id = aws_route_table.tech221_oleg_privateRT.id
+}
+```
+
+### Add Security Groups for the app
+
+```
+# Create a security group for App EC2
+
+resource "aws_security_group" "tech221_oleg_AppSG" {
+  name        = "security group for App"
+  description = "security group for App"
+  vpc_id      = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "port3000"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "tech221_oleg_AppSG"
+  }
+}
+```
+
+### Launch EC2 Instance inside Public subnet
+
+```
+resource "aws_instance" "app_instance"{
+	# which ami to use
+	ami = var.aim_id
+	
+	#type of instance
+	instance_type = "t2.micro"
+
+	# do you need the public IP
+	associate_public_ip_address = true
+
+	# Which subnet
+
+	subnet_id = "${aws_subnet.tech221_oleg_publicSubnet.id}"
+
+        # Add security groups
+
+	security_groups = ["${aws_security_group.tech221_oleg_AppSG.id}"]
+
+	# what would you like to name it
+	tags = {
+	
+	  Name = "tech221_oleg_terraform_app"
+
+	} 
+
+}
+```
--- main.tf
+++ main.tf
@@ -13,17 +13,173 @@ provider "aws" {
 }
 
 # Create a service on AWS
+
+# Create a VPC on AWS
+
+resource "aws_vpc" "tech221_oleg_terraform_vpc" {
+  cidr_block = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "tech221_oleg_terraform_vpc"
+  }
+}
+
+# create a Gateway on AWS
+resource "aws_internet_gateway" "tech221_oleg_terraform_gw" {
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  tags = {
+    Name = "tech221_oleg_terraform_gw"
+  }
+}
+
+# Create a public route table on AWS
+resource "aws_route_table" "tech221_oleg_publicRT" {
+  #default_route_table_id = aws_vpc.tech221_oleg_publicRT.default_route_table_id	
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  #route {
+    #cidr_block = var.cidr_block
+    #gateway_id = aws_internet_gateway.tech221_oleg_terraform_gw.id
+  #}
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tech221_oleg_terraform_gw.id
+  }
+
+  tags = {
+    Name = "oleg_publicRT"
+  }
+}
+
+
+# Create a private route table on AWS
+resource "aws_route_table" "tech221_oleg_privateRT" {
+  #default_route_table_id = aws_vpc.tech221_oleg_privateRT.default_route_table_id	
+  vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  #route {
+    #cidr_block = var.cidr_block
+    #gateway_id = aws_internet_gateway.tech221_oleg_terraform_gw.id
+  #}
+
+  tags = {
+    Name = "oleg_privateRT"
+  }
+}
+
+# Create a Public Subnet in VPC
+
+resource "aws_subnet" "tech221_oleg_publicSubnet" {
+	vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+	cidr_block = var.public_subnet_cidr_block
+	map_public_ip_on_launch = "true"
+	availability_zone = var.availability_zone
+
+	tags = {
+		Name = "tech221_oleg_publicSubnet"
+	}
+
+}
+
+
+# Create a Private Subnet in VPC
+
+resource "aws_subnet" "tech221_oleg_privateSubnet" {
+	vpc_id = aws_vpc.tech221_oleg_terraform_vpc.id
+	cidr_block = var.private_subnet_cidr_block
+	availability_zone = var.availability_zone
+
+	tags = {
+		Name = "tech221_oleg_privateSubnet"
+	}
+
+}
+
+
+# Creating Route association public Subnet
+
+resource "aws_route_table_association" "oleg_public_association" {
+	subnet_id = aws_subnet.tech221_oleg_publicSubnet.id
+	route_table_id = aws_route_table.tech221_oleg_publicRT.id
+}
+
+
+# Creating Route association private Subnet
+
+resource "aws_route_table_association" "oleg_private_association" {
+	subnet_id = aws_subnet.tech221_oleg_privateSubnet.id
+	route_table_id = aws_route_table.tech221_oleg_privateRT.id
+}
+
+# Create a security group for App EC2
+
+resource "aws_security_group" "tech221_oleg_AppSG" {
+  name        = "security group for App"
+  description = "security group for App"
+  vpc_id      = aws_vpc.tech221_oleg_terraform_vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  #ingress {
+    #description      = "SSH"
+    #from_port        = 22
+    #to_port          = 22
+    #protocol         = "tcp"
+    #cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  #}
+
+  ingress {
+    description      = "port3000"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "tech221_oleg_AppSG"
+  }
+}
+
 # which service - EC2
 
 resource "aws_instance" "app_instance"{
 	# which ami to use
-	ami = "ami-064b12715525b93e0"
+	ami = var.aim_id
 	
 	#type of instance
 	instance_type = "t2.micro"
 
 	# do you need the public IP
 	associate_public_ip_address = true
+
+	# Which subnet
+
+	subnet_id = "${aws_subnet.tech221_oleg_publicSubnet.id}"
+
+	# Add security groups
+
+	security_groups = ["${aws_security_group.tech221_oleg_AppSG.id}"]
 
 	# what would you like to name it
 	tags = {
@@ -33,4 +189,6 @@ resource "aws_instance" "app_instance"{
 	} 
 
 }
+
+
 
